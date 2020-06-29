4a12efe069362adc8e803e1b9140e0be75ef15b2
Ginelle Plaisir <plaisirginelle@gmail.com>
2020-06-29 08:06:08 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,12 +1,10 @@
 class CreateArtists < ActiveRecord::Migration[5.2]
-  def up
-  end
- 
-  def down
-  end
-end
-
-def change
-  create_table :artists do |t|
+  def change
+    create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
+    end
   end
 end
